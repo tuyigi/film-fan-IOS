e845b7ade51460665240742f0db50dda544091cf
Gilbert T <tuyigilbert97@gmail.com>
2021-06-23 11:55:35 +0200

    details

--- lib/models/detailsMovie.dart
+++ lib/models/detailsMovie.dart
@@ -1,1 +1,1 @@
-class DetailsMovie{  int id;  DetailsMovie({required this.id});}
+class DetailsMovie{  int id;  String title;  double popularity;  String poster_path;  String release_date;  double vote_average;  String overview;  DetailsMovie({required this.id,required this.title,required this.popularity,required this.poster_path,required this.release_date,required this.vote_average,required this.overview});}
